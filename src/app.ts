3d83ec2c9753dd35e85b81dfe723430a7d818ecb
rummangaian <mohammad.r@mobiusdtaas.ai>
2025-11-10 16:44:43 +0530

    refactor(app): register new render routes and schema definitions

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import Fastify from "fastify";
+import swaggerPlugin from "./plugins/swagger.js";
 import sensible from "@fastify/sensible";
 import renderRouter from "./api/render.router.js";
 import renderByIdRouter from "./api/render-by-id.router.js";
 import healthRouter from "./api/health.router.js";
+import renderScienceRouter from "./api/render-science.router.js";
+import renderScienceUploadRouter from "./api/render-science-upload.router.js";
 
 export function buildApp() {
   const app = Fastify({
@@ -13,8 +16,11 @@ export function buildApp() {
   });
 
   app.register(sensible);
+  app.register(swaggerPlugin);
   app.register(healthRouter);
   app.register(renderRouter);
   app.register(renderByIdRouter);
+  app.register(renderScienceRouter);
+  app.register(renderScienceUploadRouter)
   return app;
 }
